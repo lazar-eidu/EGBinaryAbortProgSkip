4632f7a1eaf2f929a93f4033c9e262c619d0792b
lazar-eidu <lazar.obradovic@eidu.com>
2023-05-19 13:55:29 +0200

    ✨ContentIdMapping.kt: implement thresholds

--- src/main/kotlin/com/eidu/personalization/ContentIdMapping.kt
+++ src/main/kotlin/com/eidu/personalization/ContentIdMapping.kt
@@ -3,14 +3,14 @@ package com.eidu.personalization
 import com.eidu.personalization.util.getSimpleCsvFromResource
 import java.lang.Exception
 
-data class ContentIdMapping(val index: Int, val contentId: String, val skill: Int) {
+data class ContentIdMapping(val index: Int, val contentId: String, val threshold: Float) {
     companion object {
         fun readContentIdMapping(): List<ContentIdMapping> =
-            getSimpleCsvFromResource("/content_id_lookup_v1.0.csv").map {
+            getSimpleCsvFromResource("/content_id_lookup_v1.0.1.csv").map {
                 require(it.size == 3) { "Invalid row: $it" }
 
                 try {
-                    ContentIdMapping(it[0].toInt(), it[2], it[1].toInt())
+                    ContentIdMapping(it[0].toInt(), it[2], it[1].toFloat())
                 } catch (e: Exception) {
                     throw IllegalArgumentException("Invalid row: $it", e)
                 }
