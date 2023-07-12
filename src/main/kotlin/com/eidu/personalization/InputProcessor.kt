d1b57b53ff462c2e5787bff24aa8893e1665dfd2
lazar-eidu <lazar.obradovic@eidu.com>
2023-07-12 14:35:30 +0200

    ✨ InputProcessor.kt: new input processor for base-tail model

--- src/main/kotlin/com/eidu/personalization/InputProcessor.kt
+++ src/main/kotlin/com/eidu/personalization/InputProcessor.kt
@@ -1,27 +1,50 @@
 package com.eidu.personalization
 
 import com.eidu.personalization.api.PersonalizationInput
+import com.eidu.personalization.api.UnitResult
 import com.eidu.personalization.api.UnitResultType
 
 class InputProcessor(contentIdMappings: List<ContentIdMapping>) {
     private val mappings: Map<String, ContentIdMapping> = contentIdMappings.associateBy { it.contentId }
-    private val inputArray = arrayOf(Array(HISTORY_LENGTH) { FloatArray(mappings.size + 1) { -1f } })
 
-    fun toTensorflowInput(input: PersonalizationInput): Array<Array<FloatArray>> {
+    fun toInitialTensorflowInput(input: PersonalizationInput): Array<FloatArray> {
+        val inputArray = Array(INPUT_LENGTH) { FloatArray(mappings.size + 1) { 0f } }
         val trimmedHistory = input.learningHistory.takeLast(HISTORY_LENGTH)
 
         trimmedHistory.forEachIndexed { historyIndex, result ->
-            val inputRow = FloatArray(mappings.size + 1)
-            mappings[result.unitId]?.let { mapping ->
-                inputRow[mapping.index] = 1f
-                inputRow[mappings.size] = if (result.resultType == UnitResultType.Success) 1f else 0f
-            }
-            inputArray[0][historyIndex] = inputRow
+            val inputRow = getInputRow(result)
+            inputArray[HISTORY_LENGTH - trimmedHistory.size + historyIndex] = inputRow
         }
         return inputArray
     }
 
+    fun toSimulatedTensorflowInput(
+        tensorflowInputBase: Array<FloatArray>,
+        simulatedTails: List<UnitResult>,
+    ): Array<FloatArray> {
+        val inputArray = Array(INPUT_LENGTH) { FloatArray(mappings.size + 1) { 0f } }
+
+        tensorflowInputBase.forEachIndexed { index, row ->
+            inputArray[index] = row
+        }
+
+        simulatedTails.forEachIndexed { index, result ->
+            val inputRow = getInputRow(result)
+            inputArray[HISTORY_LENGTH - 1 + index] = inputRow
+        }
+        return inputArray
+    }
+    private fun getInputRow(result: UnitResult): FloatArray {
+        val inputRow = FloatArray(mappings.size + 1)
+        mappings[result.unitId]?.let { mapping ->
+            inputRow[mapping.index] = 1f
+            inputRow[mapping.index] = 1f
+            inputRow[mappings.size] = if (result.resultType == UnitResultType.Success) 1f else 0f
+        }
+        return inputRow
+    }
     companion object {
         const val HISTORY_LENGTH = 50
+        const val INPUT_LENGTH = 1500
     }
 }
--- src/test/kotlin/com/eidu/personalization/InputProcessorTest.kt
+++ src/test/kotlin/com/eidu/personalization/InputProcessorTest.kt
@@ -13,28 +13,28 @@ class InputProcessorTest {
     private val processor = InputProcessor(contentIdMapping)
 
     @Test
-    fun `returns default float matrix on empty input`() {
+    fun `returns default initial matrix on empty input`() {
         val availableUnits = listOf<String>()
         val history = listOf<UnitResult>()
-        val modelInput = processor.toTensorflowInput(PersonalizationInput(history, availableUnits))
+        val modelInput = processor.toInitialTensorflowInput(PersonalizationInput(history, availableUnits))
 
         assertThat(
-            modelInput[0].all {
+            modelInput.all {
                 it.all {
-                    it == -1f
+                    it == 0f
                 }
             }
         )
-        assertThat(modelInput[0].size).isEqualTo(InputProcessor.HISTORY_LENGTH)
+        assertThat(modelInput.size).isEqualTo(InputProcessor.INPUT_LENGTH)
         assertThat(
-            modelInput[0].all {
+            modelInput.all {
                 it.size == contentIdMapping.size + 1
             }
         )
     }
 
     @Test
-    fun `returns correct float matrix when history has two units`() {
+    fun `returns correct initial matrix when history has two units`() {
         val availableUnits = listOf<String>()
         val history = listOf(
             UnitResult(
@@ -47,29 +47,42 @@ class InputProcessorTest {
                 "Anton:/../c-mat-1-us/topic-01-numbers-and-counting/block-01-review-counting-1-to-10/level-02",
                 UnitResultType.Abort,
                 10,
-                0F
+                0.5F
             )
         )
-        val modelInput = processor.toTensorflowInput(PersonalizationInput(history, availableUnits))
+        val modelInput = processor.toInitialTensorflowInput(PersonalizationInput(history, availableUnits))
 
         val expectedFirstRow = FloatArray(contentIdMapping.size + 1)
         expectedFirstRow[1] = 1f
         expectedFirstRow[contentIdMapping.size] = 1f
-        assertThat(modelInput[0][0]).isEqualTo(expectedFirstRow)
+
+        assertThat(modelInput[InputProcessor.HISTORY_LENGTH - 2]).isEqualTo(expectedFirstRow)
 
         val expectedSecondRow = FloatArray(contentIdMapping.size + 1)
         expectedSecondRow[1] = 1f
         expectedSecondRow[contentIdMapping.size] = 0f
-        assertThat(modelInput[0][1]).isEqualTo(expectedSecondRow)
 
-        val expectedDefaultRow = FloatArray(contentIdMapping.size + 1) { -1f }
-        assertThat(modelInput[0].slice(2..InputProcessor.HISTORY_LENGTH - 1)).each {
+        assertThat(modelInput[InputProcessor.HISTORY_LENGTH - 1]).isEqualTo(expectedSecondRow)
+
+        val expectedDefaultRow = FloatArray(contentIdMapping.size + 1) { 0f }
+
+        assertThat(
+            modelInput.slice(0..InputProcessor.HISTORY_LENGTH - 3)
+        ).each {
+            it.isEqualTo(expectedDefaultRow)
+        }
+
+        assertThat(
+            modelInput.slice(
+                InputProcessor.HISTORY_LENGTH..InputProcessor.INPUT_LENGTH - 1
+            )
+        ).each {
             it.isEqualTo(expectedDefaultRow)
         }
     }
 
     @Test
-    fun `returns correct float matrix when history is long`() {
+    fun `returns correct initial matrix when history is long`() {
         val availableUnits = listOf<String>()
         val subHistory = listOf(
             UnitResult(
@@ -82,24 +95,39 @@ class InputProcessorTest {
                 "Anton:/../c-mat-1-us/topic-01-numbers-and-counting/block-01-review-counting-1-to-10/level-02",
                 UnitResultType.Abort,
                 10,
-                0F
+                0.5F
             )
         )
         val history = (1..100).flatMap { subHistory }
-        val modelInput = processor.toTensorflowInput(PersonalizationInput(history, availableUnits))
-
+        val modelInput = processor.toInitialTensorflowInput(PersonalizationInput(history, availableUnits))
+        assertThat(
+            modelInput.all {
+                it.all {
+                    it != -1f
+                }
+            }
+        )
         val expectedFirstRow = FloatArray(contentIdMapping.size + 1)
         expectedFirstRow[1] = 1f
         expectedFirstRow[contentIdMapping.size] = 1f
-        assertThat(modelInput[0].slice(0..InputProcessor.HISTORY_LENGTH - 1 step 2)).each {
+        assertThat(modelInput.slice(0..InputProcessor.HISTORY_LENGTH - 1 step 2)).each {
             it.isEqualTo(expectedFirstRow)
         }
 
         val expectedSecondRow = FloatArray(contentIdMapping.size + 1)
         expectedSecondRow[1] = 1f
         expectedSecondRow[contentIdMapping.size] = 0f
-        assertThat(modelInput[0].slice(1..InputProcessor.HISTORY_LENGTH - 1 step 2)).each {
+        assertThat(modelInput.slice(1..InputProcessor.HISTORY_LENGTH - 1 step 2)).each {
             it.isEqualTo(expectedSecondRow)
+        }
+
+        val expectedDefaultRow = FloatArray(contentIdMapping.size + 1) { 0f }
+        assertThat(
+            modelInput.slice(
+                InputProcessor.HISTORY_LENGTH..InputProcessor.INPUT_LENGTH - 1
+            )
+        ).each {
+            it.isEqualTo(expectedDefaultRow)
         }
     }
 }
