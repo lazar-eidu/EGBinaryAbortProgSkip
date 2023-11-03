58de11bfb1b31e729056a986fdee499aeb58e14f
lazar-eidu <lazar.obradovic@eidu.com>
2023-11-03 17:06:01 +0100

    PluginImplementation.kt: DS34 version of PluginImplementation

--- src/main/kotlin/com/eidu/personalization/PluginImplementation.kt
+++ src/main/kotlin/com/eidu/personalization/PluginImplementation.kt
@@ -4,7 +4,6 @@ import com.eidu.personalization.api.PersonalizationInput
 import com.eidu.personalization.api.PersonalizationOutput
 import com.eidu.personalization.api.PersonalizationPlugin
 import com.eidu.personalization.api.TensorflowInferenceRunner
-import com.eidu.personalization.api.UnitResult
 import com.eidu.personalization.api.UnitResultType
 
 class PluginImplementation : PersonalizationPlugin {
@@ -29,7 +28,9 @@ class PluginImplementation : PersonalizationPlugin {
         val filteredInference = initialInference.filter { it.value < (thresholdMapping[it.key] ?: 1f) }
         if (filteredInference.isEmpty()) return PersonalizationOutput(listOf(), filteredInference)
 
-        val simulatedInferences = getSimulatedInferences(
+        val simulatedInferences = SimulatedInference(
+            modelFile, contentIdMapping, inputProcessor, outputProcessor
+        ).getSimulatedInferences(
             initialTensorflowInput, initialInference, sanitizedInput, runTensorflowInference
         )
         val maxGain = simulatedInferences.maxByOrNull { it.second }
@@ -40,51 +41,7 @@ class PluginImplementation : PersonalizationPlugin {
         )
     }
 
-    private fun getSimulatedInferences(
-        initialTensorflowInput: Array<FloatArray>,
-        initialInference: Map<String, Float>,
-        sanitizedInput: PersonalizationInput,
-        runTensorflowInference: TensorflowInferenceRunner
-    ): List<Pair<String, Float>> {
 
-        val simulatedTails = initialInference.keys.map { contentId ->
-            val simulatedTail = UnitResult(contentId, UnitResultType.Success, 0L, initialInference[contentId])
-            simulatedTail
-        }
-
-        val simulatedTensorflowOutput: Array<FloatArray> =
-            getSimulatedTensorflowOutput(
-                initialTensorflowInput, simulatedTails, runTensorflowInference
-            ).sliceArray(
-                InputProcessor.HISTORY_LENGTH - 1 until InputProcessor.HISTORY_LENGTH - 1 + simulatedTails.size
-            )
-
-        val simulatedOutputs = simulatedTensorflowOutput.zip(simulatedTails).map { (output, tail) ->
-            outputProcessor.fromTensorflowOutput(sanitizedInput.availableUnits - tail.unitId, output)
-        }
-
-        val simulatedInferences = initialInference.keys.zip(simulatedOutputs).map { (contentId, output) ->
-            contentId to (initialInference[contentId] ?: 0.0f) * output.values.sum()
-        }
-
-        return simulatedInferences
-    }
-    private fun getSimulatedTensorflowOutput(
-        initialTensorflowInput: Array<FloatArray>,
-        simulatedTails: List<UnitResult>,
-        runTensorflowInference: TensorflowInferenceRunner
-    ): Array<FloatArray> {
-
-        val tensorflowInputBase = initialTensorflowInput.sliceArray(1 until InputProcessor.HISTORY_LENGTH)
-        val simulatedTensorflowInput = inputProcessor.toSimulatedTensorflowInput(tensorflowInputBase, simulatedTails)
-        val simulatedTensorflowOutput = Array(InputProcessor.INPUT_LENGTH - InputProcessor.HISTORY_LENGTH + 1) {
-            FloatArray(contentIdMapping.size)
-        }
-
-        runTensorflowInference.infer(modelFile, simulatedTensorflowInput, simulatedTensorflowOutput)
-
-        return simulatedTensorflowOutput
-    }
 
     private fun filterAvailableUnits(input: PersonalizationInput) =
         input.availableUnits.filter { unit ->
