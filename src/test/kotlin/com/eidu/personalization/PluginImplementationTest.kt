1a772f4db300780aa520bf392450ff230f847342
lazar-eidu <lazar.obradovic@eidu.com>
2024-01-29 10:34:51 +0100

    ✅ PluginImplementationTest.kt: test progress calculation
    (cherry picked from commit a62704d7dd86c205eadd2189877891b11da484ea)

--- src/test/kotlin/com/eidu/personalization/PluginImplementationTest.kt
+++ src/test/kotlin/com/eidu/personalization/PluginImplementationTest.kt
@@ -2,9 +2,12 @@ package com.eidu.personalization
 
 import assertk.assertThat
 import assertk.assertions.isEmpty
+import assertk.assertions.isEqualTo
 import assertk.assertions.matchesPredicate
 import com.eidu.personalization.api.PersonalizationInput
 import com.eidu.personalization.api.TensorflowInferenceRunner
+import com.eidu.personalization.api.UnitResult
+import com.eidu.personalization.api.UnitResultType
 import io.mockk.justRun
 import io.mockk.mockk
 import io.mockk.verify
@@ -47,5 +50,22 @@ class PluginImplementationTest {
 
     companion object {
         private const val ACTUAL_UNIT_FROM_CSV = "Eidu:51ec0f5e-78a2-4909-b65c-4e9aae441db5/Time21"
+    }
+
+    @Test
+    fun `test getProgress`() {
+        val availableUnits = listOf("U1", "U2", "U3", "U4", "U5", "U6", "U7", "U8", "U9", "U10")
+
+        val learningHistory = listOf(
+            UnitResult("U1", UnitResultType.Success, 1000, 0.1f),
+            UnitResult("U2", UnitResultType.Success, 1500, 0.06f),
+            UnitResult("U3", UnitResultType.Success, 2000, 0.07f),
+            UnitResult("U11", UnitResultType.Success, 1200, 0.08f), // Not in available units
+            UnitResult("U12", UnitResultType.Abort, 800, 0.99f) // Not a success
+        )
+
+        val currentProgress = plugin.getProgress(PersonalizationInput(learningHistory, availableUnits))
+
+        assertThat(currentProgress).isEqualTo(0.3f) // Expected result is 3 out of 10 units meet the criteria
     }
 }
