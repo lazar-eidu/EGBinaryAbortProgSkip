479127235b2038c4c4a6d3d0b620b46ec8233f00
lazar-eidu <lazar.obradovic@eidu.com>
2024-02-06 12:12:43 +0100

    🐛 PluginConfig.kt: use javaClass in configPath
    (cherry picked from commit b7a08afee1bddd1768cd306c1af76014d2ddafa9)
    (cherry picked from commit 2adc3a4a502dce7fa0012af6e4fa1ff9a04326a4)

--- src/main/kotlin/com/eidu/personalization/PluginConfig.kt
+++ src/main/kotlin/com/eidu/personalization/PluginConfig.kt
@@ -6,8 +6,7 @@ import java.io.File
 @Serializable
 data class PluginConfig(val contentMappingPath: String, val modelPath: String) {
     companion object {
-        private val classLoader = PluginConfig::class.java.classLoader
-        val configPath: String = classLoader.getResource("config.json")?.file ?: error("config.json not found")
+        val configPath: String = javaClass.getResource("/config.json")?.file ?: error("config.json not found")
         val config: PluginConfig = Json.decodeFromString<PluginConfig>(
             File(configPath).readText()
         )
